887403b5f74c07b41521637027478ad049508037
Kailyn Williams <kailynlwilliams@gmail.com>
2018-07-28 04:08:13 -0400

    Add files via upload

---
+++ CalculatorFunctions.java
@@ -0,0 +1,25 @@
+package com.kailynwilliams;
+
+public class CalculatorFunctions {
+
+   public int multiply(int input1, int input2){
+       return input1*input2;
+    }
+
+    public int addition(int input1, int input2){
+       return input1+input2;
+    }
+
+    public int subtract(int input1, int input2){
+       return input1-input2;
+
+    }
+
+    public int divide(int input1, int input2){
+       return input1/input2;
+    }
+
+
+
+
+}
